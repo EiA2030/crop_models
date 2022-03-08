645f4e078764af7ff6e5a26897400c2b20c62c8a
egbendito <e.gbendito@gmail.com>
2022-03-08 16:07:26 +0000

    Update with conditional tryCatch() for soil data

--- DSSAT/R/1_dssat_extdata.R
+++ DSSAT/R/1_dssat_extdata.R
@@ -13,14 +13,32 @@ dssat.extdata <- function(xmin,xmax,ymin,ymax,res,sdate,edate,jobs,ex.name,path.
   dir.create(file.path(paste(getwd(), ex.name, sep = "/")))
   # Process soil & weather
   foreach::foreach(pnt=seq_along(grid[,1]), .export = '.GlobalEnv', .inorder = TRUE, .packages = c("tidyverse", "apsimx","DSSAT")) %dopar% {
-    dir.create(file.path(paste(getwd(),ex.name,paste0('EXTE', formatC(width = 4, pnt, flag = "0")), sep = "/")))
-    setwd(paste(path.to.extdata,ex.name,paste0('EXTE', formatC(width = 4, pnt, flag = "0")), sep = "/"))
+    dir.create(file.path(paste(getwd(),ex.name,paste0('EXTE', formatC(width = 4, (pnt-1), flag = "0")), sep = "/")))
+    setwd(paste(path.to.extdata,ex.name,paste0('EXTE', formatC(width = 4, (pnt-1), flag = "0")), sep = "/"))
     # read coordinates of the point
     x = grid[pnt,1]
     y = grid[pnt,2]
     ##########################################
     # Get soil ISRIC data
-    s <- get_isric_soil_profile(lonlat = c(x,y))
+    s <- tryCatch(
+      expr = {
+        apsimx::get_isric_soil_profile(lonlat = c(x,y))
+      },
+      error = function(e){
+        return(list(soil=data.frame(Depth=c(5,15,30,60,100,200),
+                                    LL15=-99,
+                                    DUL=-99,
+                                    SAT=-99,
+                                    SKS=-99,
+                                    SSS=-99,
+                                    BDM=-99,
+                                    LOC=-99,
+                                    LCL=-99,
+                                    LSI=-99,
+                                    LNI=-99,
+                                    LHW=-99)))
+      })
+    # s <- get_isric_soil_profile(lonlat = c(x,y))
     Depth<-c(5,15,30,60,100,200)
     LL15<-s$soil$LL15
     DUL<-s$soil$DUL
@@ -38,7 +56,7 @@ dssat.extdata <- function(xmin,xmax,ymin,ymax,res,sdate,edate,jobs,ex.name,path.
     write_sol(sol, "NEW.SOL", append = FALSE)
     ex_profile <- read_sol("../../NEW.SOL", id_soil = "IBPN910025")
     soilid <- ex_profile %>%
-      mutate(PEDON=paste0('TRAN', formatC(width = 6, pnt, flag = "0")),
+      mutate(PEDON=paste0('TRAN', formatC(width = 6, (pnt-1), flag = "0")),
              SLB=Depth,
              SLLL=LL15,
              SSAT=SAT,
@@ -57,11 +75,8 @@ dssat.extdata <- function(xmin,xmax,ymin,ymax,res,sdate,edate,jobs,ex.name,path.
     weathRman::get_nasa_power(lat = y, long = x,
                               start = sdate, end = edate) %>%
       {attr(.,"comments") <- str_c("! ", attr(., "comments")); .} %>%
-      DSSAT::write_wth(paste0("WHTE", formatC(width = 4, pnt, flag = "0"), ".WTH"))
+      DSSAT::write_wth(paste0("WHTE", formatC(width = 4, (pnt-1), flag = "0"), ".WTH"))
     setwd(path.to.extdata)
+    gc()
   }
 }
-
-dssat.extdata(xmin = 37, xmax = 38, ymin = 0, ymax = 1, res = 0.5,
-              sdate = "2021-01-01", edate = "2021-12-31",
-              jobs = 2, ex.name = "test_simulation", path.to.extdata = "path/to/extdata/")
