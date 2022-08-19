17ccd83be62e1376735c293f16bf09b000e19964
EduardoGB <e.gbendito@gmail.com>
2022-08-19 14:22:45 +0000

    update 2_dssat_expfile.R

--- DSSAT/R/2_dssat_expfile.R
+++ DSSAT/R/2_dssat_expfile.R
@@ -1,4 +1,4 @@
-dssat.expfile <- function(xmin,xmax,ymin,ymax,res,jobs,ex.name,path.to.extdata){
+dssat.expfile <- function(xmin,xmax,ymin,ymax,res,jobs,ex.file,ex.name,path.to.extdata){
   require(doParallel)
   require(foreach)
   # Set number of parallel workers
@@ -11,21 +11,15 @@ dssat.expfile <- function(xmin,xmax,ymin,ymax,res,jobs,ex.name,path.to.extdata){
   for (x in seq(xmin,xmax,res)) {for (y in seq(ymin,ymax,res)) {grid <- rbind(grid, c(x,y))}}
   # Process Experimental Files
   foreach::foreach(pnt=seq_along(grid[,1]), .export = '.GlobalEnv', .inorder = TRUE, .packages = c("tidyverse", "DSSAT")) %dopar% {
-    #Read sample cultivar file and filter to only cultivar IF0014
-    cul <- read_cul("MZCER047.CUL") %>%
-      filter(`VAR#` == "IF0014")
-    #Read sample cultivar file and filter to only ecotype IB0002
-    eco <- read_eco("MZCER047.ECO") %>%
-      filter(`ECO#` == "IB0002")
     #Read in original FileX
-    file_x <- read_filex("ETBA8304.MZX")
+    file_x <- DSSAT::read_filex(ex.file)
     # Set the experimental directory
     setwd(paste(path.to.extdata,ex.name,paste0('EXTE', formatC(width = 4, as.integer((pnt-1)), flag = "0")), sep = "/"))
     #Make proposed changes to FileX
     file_x$FIELDS$WSTA<-paste0("WHTE", formatC(width = 4, as.integer((pnt-1)), flag = "0"))
     file_x$FIELDS$ID_SOIL<-paste0('TRAN', formatC(width = 6, as.integer((pnt-1)), flag = "0"))
     #Overwrite original FileX with new values
-    write_filex(file_x,paste0('EXTE', formatC(width = 4, as.integer((pnt-1)), flag = "0"),'.MZX'))
+    DSSAT::write_filex(file_x,paste0('EXTE', formatC(width = 4, as.integer((pnt-1)), flag = "0"),'.MZX'))
     setwd(path.to.extdata)
     gc()
   }
